1d5158aa83d5aef145e40833b9242c2e40d09659
Giorgi Gajishvili <giorgigajishvili@Giorgis-MacBook-Air.local>
2023-06-27 14:35:07 +0400

    References added

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,10 +1,15 @@
 import { Box } from "@mui/material";
 import GridViewComponent from "../../Components/ViewComponents/GridView";
 import "./HomePage.css";
+import { useEffect, useRef } from "react";
 
 const HomePage = () => {
+  const ref = useRef<any>()
+  useEffect(()=> {
+    ref.current?.scrollIntoView(0,0)
+  },[])
   return (
-    <Box component={"div"} className="home-page">
+    <Box ref={ref} component={"div"} className="home-page">
       <GridViewComponent />
     </Box>
   );
--- src/Pages/Laptops/Laptops.tsx
+++ src/Pages/Laptops/Laptops.tsx
@@ -1,10 +1,15 @@
 import { Box,  } from "@mui/material";
 import "./Laptops.css";
 import Banner from "../../Components/Banner/Banner";
+import { useEffect, useRef } from "react";
 
 const Laptops = () => {
+  const ref = useRef<any>()
+  useEffect(()=> {
+    ref.current?.scrollIntoView(0,0)
+  },[])
   return (
-    <Box component={"div"} className="laptops-page">
+    <Box ref={ref} component={"div"} className="laptops-page">
       <Box component={"p"} className="laptops-page">
         <Box style={{width: "1000px"}}>Laptops</Box>
       </Box>
